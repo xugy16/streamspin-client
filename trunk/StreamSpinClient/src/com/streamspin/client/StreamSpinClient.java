3507119828adc16a4732a11b92502298caf122fb
jenslyn42 <jenslyn42@663d462e-77c7-11de-aa81-2fc117e74d32>
2009-08-30 19:04:39 +0000

    auto commit www update

--- trunk/StreamSpinClient/src/com/streamspin/client/StreamSpinClient.java
+++ trunk/StreamSpinClient/src/com/streamspin/client/StreamSpinClient.java
@@ -210,6 +210,7 @@ public class StreamSpinClient extends Gadget<UserPreferences> implements
 	protected void makeMainLeftMenu() {
 		
 		UserInfo menuItems = null;
+		Window.alert(ssAnswer.getAnswer());
 		try{
 			menuItems = XmlParser.instance().userInfoXmlParsing(ssAnswer.getAnswer());
 		} catch (Exception e) {
