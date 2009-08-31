7a38a6831622483190ec70cb06cb653d664aab14
jenslyn42 <jenslyn42@663d462e-77c7-11de-aa81-2fc117e74d32>
2009-08-31 15:16:00 +0000

    auto commit www update

--- StreamSpinClient/src/com/streamspin/client/StreamSpinClient.java
+++ StreamSpinClient/src/com/streamspin/client/StreamSpinClient.java
@@ -347,7 +347,7 @@ public class StreamSpinClient extends Gadget<UserPreferences> implements
 		Timer timer = new Timer() {
 			public void run() {
 				if (answer.getAnswer() != null) {
-					Window.alert(test + "\n" + answer.getAnswer());
+					Window.alert(test + "\n" + "\""+answer.getAnswer()+"\"");
 					cancel();
 					if (!answer.getAnswer().isEmpty()) {
 						ArrayList<Content> content = XmlParser.instance().contentXmlParsing(answer.getAnswer());
